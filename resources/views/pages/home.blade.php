e922d830ddf9c2e6c588abb76f0a23aecf44d01f
a-mamal <mamalikidoua@gmail.com>
2026-04-10 21:14:03 +0300

    feat(home): add initial page content with site layout

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,9 @@
+<x-site-layout
+    :title="'Home'"
+    :description="'Welcome to a-mamal.dev'"
+    :headerTitle="'Home'"
+    :subtitle="'Experimental development space'">
+
+    <p>Welcome to the lab.</p>
+
+</x-site-layout>
