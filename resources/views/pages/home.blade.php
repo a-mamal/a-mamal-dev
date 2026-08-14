6fed984ba4d7d244aa906f7a719955f007684efa
Anastasia Mamalikidou <contact@a-mamal.com>
2026-08-14 13:29:37 +0300

    feat(home): add initial site introduction (#20)

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,16 +4,22 @@
     :headerTitle="'Home'"
     :subtitle="'Experimental development space'">
 
-    <p>Welcome to the lab.</p>
+    <section class="about-wrapper">
+        <h2>Welcome to a-mamal.dev.</h2>
+        <p>The sibling of a-mamal.com</p>
+    </section>
 
-    <article>
-        <h2>Why a-mamal.dev?</h2>
+    <section>
+
+        <h3>Let's talk about this site</h3>
+
+        <h4>Why a-mamal.dev?</h4>
 
         <p>
             In case you didn't know, there's already a-mamal.com but it is meant to be a minimal overview of myself, 
             more professional although still with personality. 
             The scope, however, does not allow for many details and experiments.
-            the idea of giving a-mamal.dev a different approach came to mind almost at the same time as the initial site was live.
+            The idea of giving a-mamal.dev a different approach came to mind almost at the same time as the initial site was live.
         </p>
 
         <p>
@@ -21,5 +27,27 @@
            I need it to be a place for experiments, thoughts, documentation, 
            lessons with failures and successes, and generally a more personal space to be in.
         </p>
-    </article>
+    
+
+        <h4>Why is it deployed in its first stages?</h4>
+
+        <p> I've been thinking of it almost since a-mamal.com was deployed, back when it was still in its first stages. 
+            There are a few reasons for that. Both sites were created under the concept of "learning in public", 
+            but we will see more about this in a different section. 
+
+        </p> 
+        <p>
+            I really needed a space, apart from the tools I already have 
+            to expand and show to the world more aspects of what I'm getting into.
+            Yes, I do have my methods of note-taking, but they aren't doing much being just for myself, do they?
+        </p>
+        <p>
+            So, for this site, 
+            I thought I'd hurry up and just deploy it so I can document whatever needs to be documented, 
+            or whatever I simply want to document, from the beginning.
+            I do keep documentation in README files, they live on GitHub for my projects,
+            but I really wanted more than...something that happens as it happens.
+        </p>
+
+    </section>
 </x-site-layout>
