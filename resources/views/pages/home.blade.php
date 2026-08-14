8635231a541c45cd3ef9572eb07201e1c48acd64
a-mamal <mamalikidoua@gmail.com>
2026-08-14 11:59:14 +0300

    feat(home): add initial site introduction

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,4 +6,20 @@
 
     <p>Welcome to the lab.</p>
 
+    <article>
+        <h2>Why a-mamal.dev?</h2>
+
+        <p>
+            In case you didn't know, there's already a-mamal.com but it is meant to be a minimal overview of myself, 
+            more professional although still with personality. 
+            The scope, however, does not allow for many details and experiments.
+            the idea of giving a-mamal.dev a different approach came to mind almost at the same time as the initial site was live.
+        </p>
+
+        <p>
+           Unlike a-mamal.com, I don't want a-mamal.dev to only show the finished result.
+           I need it to be a place for experiments, thoughts, documentation, 
+           lessons with failures and successes, and generally a more personal space to be in.
+        </p>
+    </article>
 </x-site-layout>
