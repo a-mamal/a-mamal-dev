5adb3f0be0ebfb87f98d49331ed7dde4763d066a
a-mamal <mamalikidoua@gmail.com>
2026-08-31 09:10:30 +0300

    feat(articles): add article controller

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::latest()->get();
+
+        return view('pages.articles', compact('articles'));
+    }
+}
