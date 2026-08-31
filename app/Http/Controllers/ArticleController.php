59ccedb983a75dd7ce37a8a97d5f9c7947e8e531
a-mamal <mamalikidoua@gmail.com>
2026-08-31 10:01:11 +0300

    feat(articles): render Markdown content

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use App\Support\MarkdownRenderer;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function index()
+    public function index(MarkdownRenderer $markdownRenderer)
     {
         $articles = Article::latest()->get();
+
+        foreach ($articles as $article) {
+            $article->content = $markdownRenderer->render($article->content);
+        }
 
         return view('pages.articles', compact('articles'));
     }
