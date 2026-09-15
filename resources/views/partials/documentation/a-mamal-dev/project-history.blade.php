87ca63f7e87865655f79ff5c09b7a8945dea070e
a-mamal <mamalikidoua@gmail.com>
2026-09-15 10:17:03 +0300

    refactor(history): use sections for media groups

--- resources/views/partials/documentation/a-mamal-dev/project-history.blade.php
+++ resources/views/partials/documentation/a-mamal-dev/project-history.blade.php
@@ -14,143 +14,183 @@
 
             <div class="project-history-media">
                 {{-- Home --}}
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/home/2026-09-15-desktop-day.png') }}"
-                        alt="a-mamal.dev home page in desktop day mode"
-                    >
-                    <figcaption>Home - Day mode</figcaption>
-                </figure>
+                <section class="project-history-media-group">
+                    <h4>Home</h4>
+                    <div class="project-history-media-items">
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/home/2026-09-15-desktop-day.png') }}"
+                                alt="a-mamal.dev home page in desktop day mode">
+                            <figcaption>Day mode</figcaption>
+                        </figure>
 
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/home/2026-09-15-desktop-night.png') }}"
-                        alt="a-mamal.dev home page in desktop night mode"
-                    >
-                    <figcaption>Home - Night mode</figcaption>
-                </figure>
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/home/2026-09-15-desktop-night.png') }}"
+                                alt="a-mamal.dev home page in desktop night mode"
+                            >
+                            <figcaption>Night mode</figcaption>
+                        </figure>
+                    </div>
+
+                </section>
 
                 {{-- Projects --}}
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/projects/2026-09-15-desktop-day.png') }}"
-                        alt="a-mamal.dev projects page in desktop day mode"
-                    >
-                    <figcaption>Projects - Day mode</figcaption>
-                </figure>
+                <section class="project-history-media-group">
+                    <h4>Projects</h4>
+                    <div class="project-history-media-items">
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/projects/2026-09-15-desktop-day.png') }}"
+                                alt="a-mamal.dev projects page in desktop day mode"
+                            >
+                            <figcaption>Day mode</figcaption>
+                        </figure>
 
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/projects/2026-09-15-desktop-night.png') }}"
-                        alt="a-mamal.dev projects page in desktop night mode"
-                    >
-                    <figcaption>Projects - Night mode</figcaption>
-                </figure>
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/projects/2026-09-15-desktop-night.png') }}"
+                                alt="a-mamal.dev projects page in desktop night mode"
+                            >
+                            <figcaption>Night mode</figcaption>
+                        </figure>
+                    </div>
+                </section>
 
                 
                 {{-- Articles --}}
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/articles/2026-09-15-desktop-day.png') }}"
-                        alt="a-mamal.dev articles page in desktop day mode"
-                    >
-                    <figcaption>Articles - Day mode</figcaption>
-                </figure>
+                <section class="project-history-media-group">
+                    <h4>Articles page</h4>
+                    <div class="project-history-media-items">
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/articles/2026-09-15-desktop-day.png') }}"
+                                alt="a-mamal.dev articles page in desktop day mode"
+                            >
+                            <figcaption>Day mode</figcaption>
+                        </figure>
 
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/articles/2026-09-15-desktop-night.png') }}"
-                        alt="a-mamal.dev articles page in desktop night mode"
-                    >
-                    <figcaption>Articles - Night mode</figcaption>
-                </figure>
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/articles/2026-09-15-desktop-night.png') }}"
+                                alt="a-mamal.dev articles page in desktop night mode"
+                            >
+                            <figcaption>Night mode</figcaption>
+                        </figure>
+                    </div>
+                </section>
 
     
                 {{-- Article --}}
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/article/2026-09-15-desktop-day.png') }}"
-                        alt="a-mamal.dev article page in desktop day mode"
-                    >
-                    <figcaption>Article - Day mode</figcaption>
-                </figure>
+                <section class="project-history-media-group">
+                    <h4>Article page</h4>
+                    <div class="project-history-media-items">
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/article/2026-09-15-desktop-day.png') }}"
+                                alt="a-mamal.dev article page in desktop day mode"
+                            >
+                            <figcaption>Day mode</figcaption>
+                        </figure>
 
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/article/2026-09-15-desktop-night.png') }}"
-                        alt="a-mamal.dev article page in desktop night mode"
-                    >
-                    <figcaption>Article - Night mode</figcaption>
-                </figure>
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/article/2026-09-15-desktop-night.png') }}"
+                                alt="a-mamal.dev article page in desktop night mode"
+                            >
+                            <figcaption>Night mode</figcaption>
+                        </figure>
+                    </div>
+                </section>
 
                 {{-- Lab --}}
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/lab/2026-09-15-desktop-day.png') }}"
-                        alt="a-mamal.dev lab page in desktop day mode"
-                    >
-                    <figcaption>Lab - Day mode</figcaption>
-                </figure>
+                <section class="project-history-media-group">
+                    <h4>Lab page</h4>
+                    <div class="project-history-media-items">
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/lab/2026-09-15-desktop-day.png') }}"
+                                alt="a-mamal.dev lab page in desktop day mode"
+                            >
+                            <figcaption>Day mode</figcaption>
+                        </figure>
 
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/lab/2026-09-15-desktop-night.png') }}"
-                        alt="a-mamal.dev lab page in desktop night mode"
-                    >
-                    <figcaption>Lab - Night mode</figcaption>
-                </figure>
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/lab/2026-09-15-desktop-night.png') }}"
+                                alt="a-mamal.dev lab page in desktop night mode"
+                            >
+                            <figcaption>Night mode</figcaption>
+                        </figure>
+                    </div>
+                </section>
 
                 {{-- About --}}
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/about/2026-09-15-desktop-day.png') }}"
-                        alt="a-mamal.dev about page in desktop day mode"
-                    >
-                    <figcaption>About - Day mode</figcaption>
-                </figure>
+                <section class="project-history-media-group">
+                    <h4>About page</h4>
+                    <div class="project-history-media-items">
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/about/2026-09-15-desktop-day.png') }}"
+                                alt="a-mamal.dev about page in desktop day mode"
+                            >
+                            <figcaption>Day mode</figcaption>
+                        </figure>
 
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/about/2026-09-15-desktop-night.png') }}"
-                        alt="a-mamal.dev about page in desktop night mode"
-                    >
-                    <figcaption>About - Night mode</figcaption>
-                </figure>
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/about/2026-09-15-desktop-night.png') }}"
+                                alt="a-mamal.dev about page in desktop night mode"
+                            >
+                            <figcaption>Night mode</figcaption>
+                        </figure>
+                    </div>
+                </section>
 
                 {{-- Documentation --}}
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/documentation/2026-09-15-desktop-day.png') }}"
-                        alt="a-mamal.dev documentation page in desktop day mode"
-                    >
-                    <figcaption>Documentation - Day mode</figcaption>
-                </figure>
+                <section class="project-history-media-group">
+                    <h4>Documentation page</h4>
+                    <div class="project-history-media-items">
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/documentation/2026-09-15-desktop-day.png') }}"
+                                alt="a-mamal.dev documentation page in desktop day mode"
+                            >
+                            <figcaption>Day mode</figcaption>
+                        </figure>
 
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/documentation/2026-09-15-desktop-night.png') }}"
-                        alt="a-mamal.dev documentation page in desktop night mode"
-                    >
-                    <figcaption>Documentation - Night mode</figcaption>
-                </figure>
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/documentation/2026-09-15-desktop-night.png') }}"
+                                alt="a-mamal.dev documentation page in desktop night mode"
+                            >
+                            <figcaption>Night mode</figcaption>
+                        </figure>
+                    </div>
+                </section>
 
                 {{-- Documentation for a-mamal-dev --}}
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/documentation/a-mamal-dev/2026-09-15-desktop-day.png') }}"
-                        alt="a-mamal.dev project documentation in desktop day mode"
-                    >
-                    <figcaption>Documentation - a-mamal.dev - Day mode</figcaption>
-                </figure>
+                <section class="project-history-media-group">
+                    <h4>Documentation for a-mamal.dev page</h4>
+                    <div class="project-history-media-items">
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/documentation/a-mamal-dev/2026-09-15-desktop-day.png') }}"
+                                alt="a-mamal.dev project documentation in desktop day mode"
+                            >
+                            <figcaption>Day mode</figcaption>
+                        </figure>
 
-                <figure>
-                    <img
-                        src="{{ asset('history-media/a-mamal-dev/documentation/a-mamal-dev/2026-09-15-desktop-night.png') }}"
-                        alt="a-mamal.dev project documentation in desktop night mode"
-                    >
-                    <figcaption>Documentation - a-mamal.dev - Night mode</figcaption>
-                </figure>
+                        <figure>
+                            <img
+                                src="{{ asset('history-media/a-mamal-dev/documentation/a-mamal-dev/2026-09-15-desktop-night.png') }}"
+                                alt="a-mamal.dev project documentation in desktop night mode"
+                            >
+                            <figcaption>Night mode</figcaption>
+                        </figure>
+                    </div>
+                </section>
             </div> {{-- class="project-history-media" --}}
-        </article>
+        </article> {{-- class="project-history-entry" --}}
     </div> {{-- class="project-history-timeline" --}}
 </section>
